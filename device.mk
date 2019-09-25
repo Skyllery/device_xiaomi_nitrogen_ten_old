f7582db9e2bd3d46422d979f2011c20e225033f1
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-09-25 15:21:09 +0200

    make nitrogen

--- device.mk
+++ device.mk
@@ -120,7 +120,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/audio/tas2559_aac_r.ftcfg:$(TARGET_COPY_OUT_VENDOR)/etc/tas2559_aac_r.ftcfg \
+    $(LOCAL_PATH)/audio/tas2559_goer_r.ftcfg:$(TARGET_COPY_OUT_VENDOR)/etc/tas2559_goer_r.ftcfg \
+    $(LOCAL_PATH)/audio/tas2559_l.ftcfg:$(TARGET_COPY_OUT_VENDOR)/etc/tas2559_l.ftcfg
+
 
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_audio_policy_configuration.xml \
