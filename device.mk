4515547d7bf75cbeba00b61128691db923696f94
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-10-06 13:21:19 +0200

    fix vndk

--- device.mk
+++ device.mk
@@ -439,14 +439,7 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
 # VNDK
-# Update this list with what each blob is actually for
-# libicuuc: vendor.qti.hardware.qteeconnector@1.0-impl
-# libstdc++: camera.sdm660
-PRODUCT_PACKAGES += \
-    libicuuc.vendor \
-    libstdc++.vendor \
-    vndk_package
-
+PRODUCT_PACKAGES += vndk-sp
 
 # Wifi
 PRODUCT_PACKAGES += \
