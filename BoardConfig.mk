7ce2fa793f237c970e071210a1ce1ab42601167a
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-10-06 08:04:50 +0200

    Revert Compile libhwui in perf mode

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,9 +23,6 @@
 
 DEVICE_PATH := device/xiaomi/nitrogen
 
-# Compile libhwui in performance mode
-HWUI_COMPILE_FOR_PERF := true
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm660
 TARGET_NO_BOOTLOADER := true
