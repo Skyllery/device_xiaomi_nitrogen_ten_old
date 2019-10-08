926bfdc3c47adc022170a579f2cdbad5dc4880c3
GismoBe <52715540+GismoBe@users.noreply.github.com>
2019-10-08 21:29:11 +0200

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -89,8 +89,4 @@ patchelf --add-needed libprocessgroup.so "$BLOB_ROOT"/vendor/lib64/hw/sound_trig
 # Load camera.sdm660.so shim
 CAM_SDM660="$DEVICE_BLOB_ROOT"/vendor/lib/hw/camera.sdm660.so
 patchelf --add-needed camera.sdm660_shim.so "$CAM_SDM660"
-
-patchelf --replace-needed "libicuuc.so" "libicuuq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libMiWatermark.so
-patchelf --replace-needed "libminikin.so" "libminiq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libMiWatermark.so
-patchelf --set-soname "libicuuq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libicuuc.so
-patchelf --set-soname "libminiq.so" "$DEVICE_BLOB_ROOT"/vendor/lib/libminikin.so
+patchelf --replace-needed libMiWatermark.so libMiWatermark_shim.so "$CAM_SDM660" 
